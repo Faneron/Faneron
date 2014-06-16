ca2718cb22442e9f1dcd0851ab79c38eba0c82b4
Max Wang <maxwang7@gmail.com>
2014-06-16 02:18:14 -0700

    Finish Model docs and implementation of fields

--- models/comment.js
+++ models/comment.js
@@ -2,126 +2,38 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+
 var commentSchema = new Schema({
-	userID: Schema.Types.ObjectId, // User that posted this comment
-	projectID: Schema.Types.ObjectId, // Project in which this comment was posted
+
+	userID: Schema.Types.ObjectId,
+
+	projectID: Schema.Types.ObjectId,
+
 	text: {
-		subject: String, // Subject line of an original comment
-		comment: String, // Text of the comment itself
-		original: Boolean, // Indicates whether original comment or not
+		subject: String,
+		comment: String,
+		original: Boolean,
 	},
+
 	vote: {
-		votes: Number,
+		votes: {type: Number, default: 0}
 		upvoters: [Schema.Types.ObjectId],
 		downvoters: [Schema.Types.ObjectId]
 	},
-	timestamp: {type: Date, default: Date.now}, // Time the comment was posted; defaults to the time it was created
-	deleted: {type: Boolean, default: false},  // Whether the user has deleted the post; default is false
-	flag: {type: Boolean, default: false} // Marks the comment as spam -- maybe a number? -- will we have to know who marked it?
+
+	timestamp: {type: Date, default: Date.now},
+
+	deleted: {type: Boolean, default: false},
+
+	flag: {type: Boolean, default: false}
 });
-
-
-
-
 
 var Comment = mongoose.model('Comment', commentSchema);
 
 exports.Comment = Comment;
 
 
-/* Schema: Comment
- * ---------------
- * Description: Each comment represents a message that a user has
- * posted in a project. There are two types of comments: 1) Original
- * comments, and 2) regular comments. Original comments simply have
- * an additional subject line. Each comment belongs to a certain
- * user, and all comments that have been posted by a user can be 
- * displayed in one page. The comments can be filtered by time and
- * popularity (measured in terms of upvotes).
- *
- * Fields: userID, projectID
- * -------------------------
- * userID: Required
- *		Type: Schema.Types.ObjectId
- *		Description: The ID of the user that posted the comment.
- *		Default: N/A
- *
- * projectID: Required
- *		Type: Schema.Types.ObjectId
- * 		Description: The ID of the project on which the comment is posted.
- *		Default: N/A
- * 
- * Field: text
- * Sub-fields: subject, comment, original
- * --------------------------------------
- * text: Required
- *		Type: Object
- *		Description: Contains the comment text and, if applicable, the subject line.
- *			Also contains a boolean indicating whether the comment is an original
- *			comment, which determines how the comment should be displayed.
- *		Default: N/A
- *
- * ** subject: Optional
- *		Type: String
- * 		Description: The subject line text, if the comment is an original comment.
- *			If not, then subject should be undefined
- *		Default: N/A
- *
- * ** comment: Required
- *		Type: String
- * 		Description: The body/text of the comment. All comments must contain a string,
- *			even if it's an empty string.
- *		Default: ""
- *
- * ** original: Required
- *		Type: Boolean
- *		Description: Indicated whether the comment is an original comment. If it is,
- *			it should contain a subject line. True indicates that the comment is an
- *			original comment; false indicates that it isn't.
- *		Default: N/A
- *  
- * Field: Vote
- * Sub-fields: votes, upvoters, downvoters
- * ---------------------------------------
- * ** votes: Required
- * 		Type: Number
- *		Description: The vote count for a comment, which is the number of upvotes
- *			subtracted by the number of downvotes. Everytime a user up or down votes
- *			this comment, this field must be updated.
- *		Default: 0
- *
- * ** upvoters: Required
- *		Type: Array of User Object IDs
- *		Description: An array of all users that have upvoted this comment. This array
- * 			does not have to be ordered.
- *		Default: Empty array []
- *
- * ** downvoters: Required
- *		Type: Array of User Object IDs
- * 		Description: An array of all users that have downvoted this comment. This array
- *			does not have to be ordered.
- * 		Default: Empty array []
- *
- * Fields: timestamp, deleted, flag
- * --------------------------------
- * timestamp: Required
- *		Type: Date
- *		Description: The time at which the comment was created.
- *		Default: The time at which the comment was first created.
- *
- * deleted: Required
- *		Type: Boolean
- *		Description: Indicates whether the user has deleted the comment. Deleted comments are
- *			only marked as deleted, not actually removed from the database. False indicates
- *			that the comment has not been deleted; true indicates that is has.
- *		Default: False
- *
- * flag: Required
- *		Type: boolean 
- *		Description: Indicates whether the comment has been flagged as spam. True indicates that
- * 			the user has marked the comment as spam; false indicates that it hasn't.
- *		Default: False
- *
+/*
  * Function: validate_text
  * -----------------------
  * Description: Among other things, should confirm that text.original and text.subject agree
--- models/user.js
+++ models/user.js
@@ -10,11 +10,17 @@ var passwordHash = require('password-hash');
  * The following section defines the User schema and model.
  */ 
 var userSchema = new mongoose.Schema({
-	firstName: String,
-	lastName: String,
-	username: String,
-	email: String,
-	password: String
+	info: {
+		firstName: String,
+		lastName: String,
+		username: String,
+		email: String,
+		password: String
+	}
+	currency: {type: Number, default: 0},
+	xp: {type: Number, default: 0},
+	projects: [Schema.Types.ObjectId],
+	comments: [Schema.Types.ObjectId]
 });
 
 /* Function: generateHash
@@ -57,74 +63,3 @@ var loadData = function() {
 
 exports.User = mongoose.model('User', userSchema);
 exports.loadData = loadData;
-
-/* Schema: User
- * ------------
- * Description: Each User represents a Faneron user. Each User has login
- * information that is filled in while signing up. Users also have currency
- * and experience (XP) which change based upon a user's behavior. Finally,
- * users have an array of Comments that they have posted and an array of
- * Projects they have made. 
- *
- * Field: info
- * Subfields: firstName, lastName, email, username, password
- * ---------------------------------------------------------
- * info: Required
- *		Type: Object
- *		Description: Contains login information about the user
- *		Default: N/A
- * 
- * ** firstName: Required
- *		Type: String
- *		Description: The User's real first name. Cannot be an empty string.
- *		Default: N/A
- *
- * ** lastName: Required
- *		Type: String
- *		Description: The User's real last name. Cannot be an empty string.
- *		Default: N/A
- *
- * ** email: Required
- *		Type: String
- *		Description: The User's email. Cannot be an empty string.
- *		Default: N/A
- *
- * ** username: Required
- *		Type: String
- *		Description: The User's chosen username. Cannot be an empty string.
- *		Default: N/A
- *
- * ** password: Required
- *		Type: String
- *		Description: The User's chosen password. Must be at least 8 characters.
- *		Default: N/A
- *
- * Field: currency
- * ---------------
- * currency: Required
- *		Type: Number
- *		Description: The amount of currency a User has earned.
- *		Default: 0
- *
- * Field: xp
- * ---------
- * xp: Required
- *		Type: Number
- * 		Description: The number of experience points (xp) the User has.
- *		Default: 0
- *
- * Field: projects
- * ---------------
- * projects: Optional
- * 		Type: Array
- *		Description: An array of Project Object IDs created by this User.
- *		Default: N/A
- *
- * Field: comments
- * ---------------
- * comments: Optional
- * 		Type: Array
- *		Description: An array of Comment Object IDs created by this User.
- *		Default: N/A
- *
- */
