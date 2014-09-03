439566ee611fb74126028e9a997b1cd1a8090e31
Max Wang <mwang@onrisk.com>
2014-09-03 16:25:54 -0700

    Add note about using sign_aws_s3 with env vars
    The sign_aws_s3 handler requires two environment variables:
    AWS_ACCESS_KEY and AWS_SECRET_KEY.

--- routes/auth.js
+++ routes/auth.js
@@ -81,7 +81,13 @@ exports.logout = function(req, res) {
     res.send(200, {redirect: 'front'});
 };
 
+/*
+Assumes that the server was run with the environment variables:
+AWS_ACCESS_KEY 
+AWS_SECRET_KEY
+This information should come from the key-pairs provided by AWS.
 
+*/
 exports.sign_aws_s3 = function(req, res) {
     AWS.config.update({
         "accessKeyId": process.env.AWS_ACCESS_KEY,
